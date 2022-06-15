fb9ad9560e2633b964de3664cf5c8f6bbdda3e31
Artem Leonidov <leonidovartem85@gmail.com>
2022-06-15 19:31:13 +0300

    js update

--- game.js
+++ game.js
@@ -1,19 +1,100 @@
-
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
 var bird = new Image();
 var bg = new Image();
 var fg = new Image();
-var pipeup = new Image();
-var pipebottom = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
 
 bird.src = "img/bird.png";
 bg.src = "img/bg.png";
 fg.src = "img/fg.png";
-pipeup.src = "img/pipeup.png";
-pipebottom.src = "img/pipebottom.png";
+pipeUp.src = "img/pipeup.png";
+pipeBottom.src = "img/pipebottom.png";
 
+var gap = 90;
+
+document.addEventListener("keydown", moveUp);
+
+function moveUp(){
+    yPos -= 25;
+}
+
+var pipe = [];
+pipe[0] = {
+    x : cvs.width,
+    y : 0
+}
+
+
+var xPos = 10;
+var yPos = 150;
+var grav = 1.5;
+
+function draw(){
+    ctx.drawImage(bg,0,0);
+
+    for(var i = 0; i < pipe.length;i++){
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+        pipe[i].x--;
+
+        if(pipe[i].x == 125){
+            pipe.push({
+                x : cvs.width,
+                y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+       if(xPos + bird.width >= pipe[i].x
+        && xPos <= pipe[i].x + pipeUp.width
+        && (yPos <= pipe[i].y + pipeUp.height
+            || yPos + bird.height >= pipe[i].y + pipeUp.height +
+             gap)|| yPos + bird.height >= cvs.height - fg.height){
+                location.reload();
+            }
+    }
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+    requestAnimationFrame(draw);
+};
+
+pipeBottom.onload = draw;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var fly = new Audio();
 var score_audio = new Audio();
 
@@ -29,16 +110,17 @@ function moveup() {
    fly.play();
 }
 var pipe = [];
-pipe[0]{
-   x : cvs.height
-   y : 0
+pipe[0];{
+   x : cvs.height;
+   y : 0;
 }
 
 var xPos = 0;
 var yPos = 150;
 var grav = 1;
 var score = 0;
-
+*/
+/*
 function draw() {
    ctx.drawImage (bg, 0, 0);
    
@@ -75,4 +157,4 @@ function draw() {
    ctx.FillText("Score:" + score,10, cvs.height- 20)
    
    requestAnimationFrame(draw);
-   }
+   }*/
