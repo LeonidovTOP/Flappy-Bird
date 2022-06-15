925de1af4b7053af90461dd08c044832db8bc7cf
Artem Leonidov <leonidovartem85@gmail.com>
2022-06-15 22:54:25 +0300

    js game

--- game.js
+++ game.js
@@ -13,12 +13,19 @@ fg.src = "img/fg.png";
 pipeUp.src = "img/pipeup.png";
 pipeBottom.src = "img/pipebottom.png";
 
+var fly = new Audio();
+var score = new Audio();
+
+fly.src = "audio/fly.mp3";
+score.src = "audio/score.mp3";
+
 var gap = 90;
 
 document.addEventListener("keydown", moveUp);
 
 function moveUp(){
     yPos -= 25;
+    fly.play();
 }
 
 var pipe = [];
@@ -31,6 +38,8 @@ pipe[0] = {
 var xPos = 10;
 var yPos = 150;
 var grav = 1.5;
+
+ var score = 0;
 
 function draw(){
     ctx.drawImage(bg,0,0);
@@ -54,12 +63,22 @@ function draw(){
              gap)|| yPos + bird.height >= cvs.height - fg.height){
                 location.reload();
             }
+
+            if(pipe[i].x == 5){
+                score++;
+            }
+
     }
 
     ctx.drawImage(fg, 0, cvs.height - fg.height);
     ctx.drawImage(bird, xPos, yPos);
 
     yPos += grav;
+
+    ctx.fillStyle="#000000";
+    ctx.font = "24px Verdana";
+    ctx.fillText("SCORE:" + score, 10, cvs.height-20)
+
     requestAnimationFrame(draw);
 };
 
